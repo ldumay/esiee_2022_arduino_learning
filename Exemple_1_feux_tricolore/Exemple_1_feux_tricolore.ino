ab9f039cc764a60464258428ec7253d9a2a0170e
ldumay <dumay.famille@gmail.com>
2022-03-20 14:32:54 +0100

    Exemple_1_feux_tricolore

--- Exemple_1_feux_tricolore/Exemple_1_feux_tricolore.ino
+++ Exemple_1_feux_tricolore/Exemple_1_feux_tricolore.ino
@@ -3,29 +3,40 @@
 void setup()
 {
   pinMode(5, OUTPUT);
+  pinMode(10, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 void loop()
 {
+  // Feux 1
   digitalWrite(5, HIGH);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
-  delay(6000);
-  
-  digitalWrite(5, LOW);
+  // Feux 2
+  digitalWrite(10, HIGH);
+  delay(5000); // Wait for 5000 millisecond(s)
+  // Feux 2
   digitalWrite(6, HIGH);
-  digitalWrite(7, LOW);
-  delay(2000);
-  
   digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
+  delay(2000); // Wait for 2000 millisecond(s)
+  // Feux 1
   digitalWrite(7, HIGH);
-  delay(6000);
-  
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
+  digitalWrite(6, LOW);
+  // Feux 2
   digitalWrite(10, LOW);
-  delay(50);
+  digitalWrite(8, HIGH);
+  delay(5000); // Wait for 5000 millisecond(s)
+  // Feux 2
+  digitalWrite(9, HIGH);
+  digitalWrite(8, LOW);
+  delay(2000); // Wait for 2000 millisecond(s)
+  // Feux 2
+  digitalWrite(10, HIGH);
+  digitalWrite(9, LOW);
+  delay(1); // Wait for 1 millisecond(s)
+  // Feux 1
+  digitalWrite(7, LOW);
+  digitalWrite(5, HIGH);
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 2022_arduino_learning
+# Arduino - Apprentissage
+
+## Exemlpe - N°1
+![Exemple_1_feux_tricolore](images/exemple_1_feux_tricolore.jpg)
+Code : [Exemple_1_feux_tricolore](Exemple_1_feux_tricolore/Exemple_1_feux_tricolore.ino)
